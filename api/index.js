739c12960adfbb2806920c7411b961e614d0b914
nur922184 <nuruzzaman.engbd@gmail.com>
2026-05-06 12:49:12 +0600

    update

--- api/index.js
+++ api/index.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 // ================= ROUTES =================
